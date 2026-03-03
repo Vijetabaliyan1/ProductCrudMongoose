988db6c8840ef168b7f2635011f165c47f0245f6
Vijeta <vijetabaliyan3691@gmail.com>
2026-03-04 01:04:11 +0530

    Fixed folder case issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const productRoutes = require("./routes/productRoutes");
+const productRoutes = require("./Routes/productRoutes");
 
 const app = express();
 
